e597832579b3c9edd9e493cdc4f9178200ce6287
Romain C <62849703+rcontrai@users.noreply.github.com>
2021-02-12 11:40:55 +0100

    Update capture.js

--- capture.js
+++ capture.js
@@ -5,7 +5,7 @@
 
   var width = 320;    // We will scale the photo width to this
   var height = 0;     // This will be computed based on the input stream
-  var framerate = 3;
+  var framerate = 10;
 
   // |streaming| indicates whether or not we're currently streaming
   // video from the camera. Obviously, we start at false.
